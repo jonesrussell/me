d4ee403012db0f280ac94f232285349b1a3a5b7d
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-13 13:26:48 +0000

    place windows at X or Y

--- src/routes/sections/BlogCards.svelte
+++ src/routes/sections/BlogCards.svelte
@@ -1,5 +1,8 @@
-<script>
+<script lang="ts">
 	import Window from '../../components/Window/Window.svelte';
+
+	export let x: number;
+	export let y: number;
 </script>
 
-<Window title="Blog">Blogger</Window>
+<Window title="Blog" x={x} y={y}>Blogger</Window>
