11b00cd54c1621ae3334c7404b1fbca7c7f0f30f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-13 13:09:58 +0000

    prettier formatting

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="flex flex-col">
-	<div class="bg-[#cfe0fc] w-full border-b border-black flex justify-around">
+	<div class="flex w-full justify-around border-b border-black bg-[#cfe0fc]">
 		<span>Amiga Workbench</span>
 		<span class="hidden md:block">1,912,400 graphics mem</span>
 		<span class="hidden md:block">7,686,560 other mem</span>
