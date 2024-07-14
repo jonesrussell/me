5bbb15a897e9efdb32cdc61bd516ca01644c517e
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-14 04:14:48 +0000

    Create blogService

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,8 @@
-// src/lib/types.ts
-import WinSignupNews from '../routes/sections/WinSignupNews.svelte';
-import WinBlog from '../routes/sections/WinBlog.svelte';
+import type WinSignupNews from '../routes/sections/WinSignupNews.svelte';
+import type WinBlog from '../routes/sections/WinBlog.svelte';
 
 export type Window = {
 	component: typeof WinSignupNews | typeof WinBlog;
-  
 	x: number;
 	y: number;
 };
