db6527fe24c22477a75753a4b5197421123bd2d6
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-17 12:23:36 +0000

    Update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,15 @@
 <footer class="text-center">
 	<p>
-		visit
-		<a href="https://kit.svelte.dev">kit.svelte.dev</a>
-		to learn SvelteKit
+		&copy; {new Date().getFullYear()} Russell Jones |
+		<a href="https://github.com/jonesrussell/me" target="_blank" rel="noopener noreferrer" class="underline"
+			>Source Code</a
+		>
+		| Licensed under
+		<a
+			href="https://github.com/jonesrussell/me/blob/main/LICENSE"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="underline">MIT</a
+		>
 	</p>
 </footer>
