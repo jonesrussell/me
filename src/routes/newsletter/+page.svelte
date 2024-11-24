38cbcf72e18d60cad17e8002a42b5cd006aee2cb
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-11-24 03:15:39 +0000

    footer order

--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Pane from '../../components/Pane/Pane.svelte';
-
 	export let x: number;
 	export let y: number;
 	export let id: string;
@@ -17,33 +15,40 @@
 	export let onBringToFront = () => {};
 </script>
 
-<Pane title="Newsletter" {id} {x} {y} {zIndex} {onBringToFront}>
+<svelte:head>
+	<title>Newsletter | Russell Jones</title>
+	<meta name="description" content="Sign up for my newsletter" />
+</svelte:head>
+
+<section>
+	<h1>Newsletter</h1>
+
 	<form on:submit|preventDefault={handleSubmit} class="space-y-4">
 		<div>
-			<label for="email" class="mb-2 block text-sm font-bold text-gray-700">Email:</label>
+			<label for="email" class="mb-2 block text-sm font-bold">Email:</label>
 			<input
 				id="email"
 				type="email"
 				bind:value={email}
 				required
-				class="focus:shadow-outline-blue w-full border border-gray-600 px-3 py-2 text-sm leading-tight text-gray-700 shadow-none focus:outline-none"
+				class="w-full border border-gray-600 px-3 py-2 text-sm leading-tight focus:outline-none"
 			/>
 		</div>
 		<div>
-			<label for="name" class="mb-2 block text-sm font-bold text-gray-700">Name:</label>
+			<label for="name" class="mb-2 block text-sm font-bold">Name:</label>
 			<input
 				id="name"
 				type="text"
 				bind:value={name}
 				required
-				class="focus:shadow-outline-blue w-full border border-gray-600 px-3 py-2 text-sm leading-tight text-gray-700 shadow-none focus:outline-none"
+				class="w-full border border-gray-600 px-3 py-2 text-sm leading-tight focus:outline-none"
 			/>
 		</div>
 		<button
 			type="submit"
-			class="focus:shadow-outline-blue full w-full bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+			class="w-full bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
 		>
 			Sign Up
 		</button>
 	</form>
-</Pane>
+</section>
