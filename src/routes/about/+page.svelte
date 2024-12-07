910604cc3033017835d667026c4e14613e72525f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-12-07 22:31:10 +0000

    Refactor and remove Pane components and session management
    - Deleted the Pane component and its related subcomponents (PaneHeader, PaneFooter, PaneButton) to streamline the codebase.
    - Removed session management from stores, simplifying state handling.
    - Updated layout and route components to eliminate dependencies on the removed Pane structure, enhancing overall application performance and maintainability.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-	import Pane from '../../components/Pane/Pane.svelte';
-
-	export let x: number = 100;
-  export let y: number = 150;
-  export let zIndex: number = 2;
-	export let id: string = 'about';
-
-	export let onBringToFront = () => {};
 </script>
 
 <svelte:head>
@@ -14,12 +6,10 @@
 	<meta name="description" content="About Russell" />
 </svelte:head>
 
-<Pane title="About" {id} {x} {y} {zIndex} {onBringToFront}>
-	<div class="text-column">
-		<h1>About Russell</h1>
-	
-		<p>
-			Me dev.
-		</p>
-	</div>
-</Pane>
+<div class="text-column">
+	<h1>About Russell</h1>
+
+	<p>
+		Me dev.
+	</p>
+</div>
