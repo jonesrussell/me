09bfc95a4377e5322b8fc3e5907bd415925f47ef
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-04-16 18:31:20 +0000

    Refactor configuration files and improve testing setup for consistency and clarity
    - Reformatted package.json for consistent indentation and style.
    - Updated playwright.config.ts and vitest.config.ts for improved readability and organization.
    - Enhanced test cases in Terminal.test.ts to ensure accurate rendering and functionality checks.
    - Adjusted GitHub Actions workflow for Playwright to maintain consistent formatting.

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,7 +17,9 @@ export default defineConfig({
 		alias: {
 			$lib: fileURLToPath(new URL('./src/lib', import.meta.url)),
 			$app: fileURLToPath(new URL('./node_modules/@sveltejs/kit/src/runtime/app', import.meta.url)),
-			__sveltekit: fileURLToPath(new URL('./node_modules/@sveltejs/kit/src/runtime', import.meta.url))
+			__sveltekit: fileURLToPath(
+				new URL('./node_modules/@sveltejs/kit/src/runtime', import.meta.url)
+			)
 		},
 		conditions: ['browser']
 	},
